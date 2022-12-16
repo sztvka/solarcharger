306174953e954ec1d683690a659da476e7e8ebda
sztvka <sztukaa@protonmail.com>
2022-12-16 22:28:35 +0100

    servo??

--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 #include <esp_log.h>
 #include <assert.h>
 #include <wifi.h>
+#include <iot_servo.h>
+
 
 #define I2C_PORT 0
 
 #define I2C_SCL 22
 #define I2C_SDA 21
+
+
+#define SERVO_1 25
+#define SERVO_2 26
 
 const uint8_t INA219_1_ADDR = 0x40; //solar
 const uint8_t INA219_2_ADDR = 0x44; //esp32
@@ -20,6 +26,26 @@ const uint8_t INA219_2_ADDR = 0x44; //esp32
 #define MAX_CUR 1
 
 
+servo_config_t servo_cfg = {
+    .max_angle = 180,
+    .min_width_us = 500,
+    .max_width_us = 2500,
+    .freq = 50,
+    .timer_number = LEDC_TIMER_0,
+    .channels = {
+        .servo_pin = {
+            SERVO_1,
+            SERVO_2,
+        },
+        .ch = {
+            LEDC_CHANNEL_0,
+            LEDC_CHANNEL_1,
+        },
+    },
+    .channel_number = 2,
+} ;
+
+float angle = 100.0f;
 
 
 void ina_measure(void *addr)
@@ -52,6 +78,15 @@ void ina_measure(void *addr)
     }
 }
 
+void servo_loop(){
+    while(true){
+        for(int i = 0; i<180; i=i+10){
+            iot_servo_write_angle(LEDC_LOW_SPEED_MODE, 0, i);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+    }
+}
+
 void app_main()
 {
     ESP_ERROR_CHECK(i2cdev_init());
@@ -62,6 +97,10 @@ void app_main()
     setINA219_2(0.2,0.2,0.2);
     xTaskCreate(ina_measure, "INA219_1", configMINIMAL_STACK_SIZE * 8, (void*)&INA219_1_ADDR, 5, NULL);
     xTaskCreate(ina_measure, "INA219_2", configMINIMAL_STACK_SIZE * 8, (void*)&INA219_2_ADDR, 5, NULL);
+    iot_servo_init(LEDC_LOW_SPEED_MODE, &servo_cfg);
+    xTaskCreate(servo_loop, "servo", configMINIMAL_STACK_SIZE*4, NULL, 4, NULL);
+    
+  //  xTaskCreate(servo, "ServoHandle", configMINIMAL_STACK_SIZE * 5, NULL, 4, NULL);
     printf("Free memory left: %u", esp_get_free_heap_size());
 
 }
