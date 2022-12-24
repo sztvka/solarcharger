b5e14f9d3f7919398555920cf18f4ee693be06a5
sztvka <sztukaa@protonmail.com>
2022-12-24 15:09:20 +0100

    apexcharts testing

--- data/main.js
+++ data/main.js
@@ -39,6 +39,9 @@ updateGauges = (obj) =>{
 };
 
 
+
+
+
 fetchData = () => {
     fetch('data', {
         method: 'GET',
@@ -55,7 +58,98 @@ fetchData = () => {
 
 window.onload = function() {
     document.getElementsByClassName("mask")[0].style = "visibility: hidden";;
-    setInterval(fetchData, 1000);
+  //  setInterval(fetchData, 1000);
 };
 
+
+function getNewData(baseval, yrange) {
+  var newTime = baseval+3600000;
+  return {
+    x: newTime,
+    y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
+  };
+}
+
+window.Apex = {
+  tooltip: {
+    theme: "dark",
+    x: {
+      formatter: function (val) {
+        var d = new Date(val);
+        var min = () => {
+          if (d.getUTCMinutes()<10){
+            return "0"+d.getUTCMinutes();
+          }
+          else return d.getUTCMinutes();
+        }
+        var parsed = d.getUTCHours()+":"+min()+":"+d.getUTCSeconds();
+        return parsed
+      }
+    }
+  }
+}
+
+
+var data = [];
+var options = {
+  series: [{
+    name: "Power",
+    data: data.slice()
+}],
+  chart: {
+    id: 'realtime',
+    height: 350,
+    type: 'line',
+    animations: {
+      enabled: true,
+      easing: 'linear',
+      dynamicAnimation: {
+        speed: 1000
+      }
+    },
+    toolbar: {
+      show: false
+    },
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'smooth'
+  },
+  title: {
+    text: 'Dynamic Updating Chart',
+    align: 'left'
+  },
+  markers: {
+    size: 0
+  },
+  xaxis: {
+    type: 'datetime',
+    range: 5000,
+  },
+  yaxis: {
+    max: 40
+  },
+  legend: {
+    show: false
+  },
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+window.setInterval(function () {
+  var b = {};
+  b = getNewData(Date.now(), {min: 0, max:30});
+  data.push([b.x,b.y]);
+
+chart.updateSeries([{
+  data: data
+}])
+}, 1000)
 
