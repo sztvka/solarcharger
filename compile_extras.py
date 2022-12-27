15c9fe9bc77c82162122cbeb21a01fc9a33bf1f7
sztvka <sztukaa@protonmail.com>
2022-12-27 01:01:02 +0100

    working on compile_extras

--- compile_extras.py
+++ compile_extras.py
@@ -1,10 +1,25 @@
 #Script used for setting config and minifying web files before loading them onto spiffs.
 
+Import("env")
+env.Execute("$PYTHONEXE -m pip install css-html-js-minify rjsmin")
+
 import shutil
 import os
 
+try:
+    from css_html_js_minify import process_single_html_file, process_single_css_file
+except ImportError:
+    env.Execute("$PYTHONEXE -m pip install css-html-js-minify")
+try:
+    import rjsmin
+except ImportError:
+    env.Execute("$PYTHONEXE -m pip install rjsmin")
+
+
 config_present = os.path.isfile('lib/config/config.h')
 dir_present = os.path.isdir('lib/config')
+minified_data = 'data_minified'
+regular_data = 'data'
 
 if config_present:
     print("Config file found, proceeding...")
@@ -18,8 +33,9 @@ else:
 
 
 print("Minifying data files....")
-minified_data = 'data_minified'
-regular_data = 'data'
+
+if not os.path.isdir(minified_data):
+    os.mkdir(minified_data)
 for filename in os.listdir(minified_data):
     file_path = os.path.join(minified_data, filename)
     try:
@@ -37,22 +53,20 @@ for filename in os.listdir(regular_data):
     if os.path.isfile(file_path):
         if "favicon" in filename:
             shutil.copy2(file_path, minified_path)
-        elif "chart" in filename:
-            shutil.copy2(file_path, minified_path)
-        elif "bootstrap" in filename:
+        elif ".min" in filename:
             shutil.copy2(file_path, minified_path)
         else:
-            if os.name == 'nt':
-                if ".js" in filename:
-                    runstring = ".\minify " + file_path + " -o " + minified_path
-                    os.system(runstring)
-                else:
-                    runstring = ".\minify --minify-css " + file_path + " -o " + minified_path
-                    os.system(runstring)
-            else:
-                 if ".js" in filename:
-                    runstring = "./minify " + file_path + " -o " + minified_path
-                    os.system(runstring)
-                 else:
-                    runstring = "./minify --minify-css " + file_path + " -o " + minified_path
-                    os.system(runstring)
+            if ".js" in filename:
+                jsFile = open(file_path, "r")
+                data = jsFile.read()
+                jsFile.close()
+                minified = rjsmin.jsmin(data)
+                output = open(minified_path, "w")
+                output.write(minified)
+                output.close()
+                print("Minified %s" % minified_path)
+            elif ".html" in filename:
+                process_single_html_file(file_path, output_path=minified_path)
+            elif ".css" in filename:
+                process_single_css_file(file_path, output_path=minified_path)
+
