294fce5a3b56d0a6a47dd4691f091303e6d72fc3
sztvka <sztukaa@protonmail.com>
2023-01-19 01:24:10 +0100

    cleanup

--- lib/spiffs/spiffs.h
+++
@@ -1,5 +0,0 @@
-void spiffs_init();
-void spiffs_test();
-char *return_html(int bufsize); 
-char *return_js(int bufsize);
-char *return_css(int bufsize);
--- lib/wifi/wifi.h
+++
@@ -1,4 +0,0 @@
-void wifi_init(void);
-void setINA219_1(float voltage, float current, float wattage);
-void setINA219_2(float voltage, float current, float wattage);
-void wifi_init_sta(void);
